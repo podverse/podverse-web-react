7c2ff6f6951eaed7ef7611e9119836145612af43
Mitch Downey <mitchldowney@gmail.com>
2018-05-14 01:08:23 -0500

    Eject create-react-app; add scss support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import './scss/reset.css';
+import './scss/reset.scss';
 import App from './ts/containers/App/App';
 
 ReactDOM.render(
